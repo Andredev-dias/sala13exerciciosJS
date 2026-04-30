15f1deed767ed96181efedabbe64ca4d0127b9ba
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-04-30 09:28:45 -0300

    Add files via upload

--- calculos/script.js
+++ calculos/script.js
@@ -5,24 +5,12 @@ function somar(){
     resp.textContent = n1 + n2
 }
 
-function sub(){
-    var n3 = document.getElementById("n3").valueAsNumber
-    var n4 = document.getElementById("n4").valueAsNumber 
-    var resp = document.getElementById("respSub")
-    resp.textContent = n3 - n4
-}
 
-function multi(){
-    var n3 = document.getElementById("n5").valueAsNumber
-    var n4 = document.getElementById("n6").valueAsNumber 
-    var resp = document.getElementById("respMulti")
-    resp.textContent = n3 * n4
-}
-
-function div(){
-    var n3 = parseFloat(document.getElementById("n7").value)
-    var n4 = parseFloat(document.getElementById("n8").value)
+function dividir(){
+    var n3 = parseFloat(document.getElementById("n3").value)
+    var n4 = parseFloat(document.getElementById("n4").value)
     var resp = document.getElementById("respDiv")
+
     if(n4 !== 0){
         resp.textContent = n3 / n4
     }else{
